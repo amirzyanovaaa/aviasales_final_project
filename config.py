b55b2115bc4c34a942ca0280777c036dd3be7de5
Камила Амирзянова <kamilaamirzanova@MacBook-Air-Kamila.local>
2025-12-24 14:21:35 +0300

    7th version

--- config.py
+++ config.py
@@ -3,11 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = '8523459252:AAFPRYvniIoPWoSiLWv8jJaaTmwvv3Q5gwE'
-AMADEUS_API_KEY = 'mYjjgWAsz3SptSG2JdK6CnvZYZRfQgJT'
-AMADEUS_API_SECRET = 'sfj3n00vw6NHVvg0'
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+AMADEUS_API_KEY = os.getenv("AMADEUS_API_KEY")
+AMADEUS_API_SECRET = os.getenv("AMADEUS_API_SECRET")
 AMADEUS_BASE_URL = os.getenv("AMADEUS_BASE_URL", "https://test.api.amadeus.com")
-DEFAULT_CURRENCY = "RUB"
-
-DB_URL = "sqlite+aiosqlite:///db.sqlite3"
-
+DEFAULT_CURRENCY = os.getenv("DEFAULT_CURRENCY", "RUB")
+DB_URL = os.getenv("DB_URL", "sqlite+aiosqlite:///db.sqlite3")
